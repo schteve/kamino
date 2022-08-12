4d9ae6d3fc4b0f7ca6b50772ee496ca4ec699861
Steve Heindel <stevenheindel@gmail.com>
2022-08-12 16:09:30 -0400

    Check uncommitted

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::{path::PathBuf, fs::read_dir};
+use git2::{Repository, StatusOptions};
+use std::{fs::read_dir, path::PathBuf};
 
 #[derive(Parser)]
 struct Args {
@@ -9,12 +10,46 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    for entry in read_dir(args.dir).expect("Given path is not a directory: {dir}") {
-        if let Ok(x) = entry {
-            let path = x.path();
-            if path.is_dir() {
-                println!("{}", path.display());
+    println!(
+        "Kamino scanning repos in {:?}",
+        args.dir.canonicalize().unwrap()
+    );
+
+    // Get all dir entries in given dir
+    let mut dirs = Vec::new();
+    for entry in read_dir(args.dir)
+        .expect("Given path is not a directory: {dir}")
+        .flatten()
+    {
+        let path = entry.path();
+        if path.is_dir() {
+            dirs.push(path);
+        }
+    }
+
+    // Check if there are any uncommitted local changes
+    for dir in dirs {
+        if let Ok(repo) = Repository::open(&dir) {
+            if check_uncommitted(&repo) {
+                println!("\tRepo in {dir:?} has uncommitted changes");
             }
         }
     }
+
+    println!("Kamino scans complete!");
+}
+
+fn check_uncommitted(repo: &Repository) -> bool {
+    let mut status_opts = StatusOptions::new();
+    status_opts.include_ignored(false).include_untracked(true);
+
+    let statuses = repo
+        .statuses(Some(&mut status_opts))
+        .expect("Error getting repo status");
+    !statuses.is_empty()
+    /*if !statuses.is_empty() {
+        for s in statuses.iter() {
+            println!("    {}", s.path().unwrap()); // TODO: use logging
+        }
+    }*/
 }
