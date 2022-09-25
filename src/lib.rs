9c57eef5517da36173fdd239b7793593b71103bb
Steve Heindel <stevenheindel@gmail.com>
2022-09-25 18:50:20 -0400

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -65,6 +65,7 @@ pub fn check_stashed(repo: &mut Repository) -> Result<u32, StashedError> {
 }
 
 /// Contains details about the state of a branch relative to the remote server.
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct AheadBehind {
     /// The number of commits this branch is ahead of the remote server, or None if no upstream branch is detected.
     pub ahead: Option<usize>,
@@ -173,6 +174,7 @@ fn git_cred_check(
 }
 
 /// Indicates the state of a single git hook.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum HookState {
     /// Only in `.git/hooks`.
     ActiveOnly,
@@ -185,6 +187,7 @@ pub enum HookState {
 }
 
 /// Contains the name and state of a single git hook.
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Hook {
     /// The filename of the git hook (the same name in `.git/hooks` and `.githooks`).
     pub name: OsString,
@@ -278,4 +281,296 @@ fn hook_filenames_in_dir(dir: &Path) -> impl Iterator<Item = OsString> + '_ {
         .filter(|path| path.is_file())
         .filter(|path| path.extension() != Some(OsStr::new("sample")))
         .filter_map(|path| path.file_name().map(ToOwned::to_owned))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use git2::{RepositoryInitOptions, StashFlags};
+    use std::{
+        fs::{self, File},
+        io::Write,
+    };
+    use tempfile::TempDir;
+
+    // Stolen from git2
+    fn repo_init() -> (TempDir, Repository) {
+        let td = TempDir::new().unwrap();
+        let mut opts = RepositoryInitOptions::new();
+        opts.initial_head("main");
+        let repo = Repository::init_opts(td.path(), &opts).unwrap();
+        {
+            let mut config = repo.config().unwrap();
+            config.set_str("user.name", "name").unwrap();
+            config.set_str("user.email", "email").unwrap();
+            let mut index = repo.index().unwrap();
+            let id = index.write_tree().unwrap();
+
+            let tree = repo.find_tree(id).unwrap();
+            let sig = repo.signature().unwrap();
+            repo.commit(Some("HEAD"), &sig, &sig, "initial\n\nbody", &tree, &[])
+                .unwrap();
+        }
+        (td, repo)
+    }
+
+    fn create_file(dir: &Path, filename: &str) {
+        create_file_with_contents(dir, filename, "contents");
+    }
+
+    fn create_file_with_contents(dir: &Path, filename: &str, contents: &str) {
+        if !dir.exists() {
+            fs::create_dir(dir).unwrap();
+        }
+        File::create(dir.join(filename))
+            .unwrap()
+            .write_all(contents.as_bytes())
+            .unwrap();
+    }
+
+    fn remove_file(dir: &Path, filename: &str) {
+        fs::remove_file(dir.join(filename)).unwrap();
+    }
+
+    fn add_file_to_index(repo: &Repository, filename: &str) {
+        let mut index = repo.index().unwrap();
+        index.add_path(&Path::new(filename)).unwrap();
+    }
+
+    fn commit_index_to_branch(repo: &Repository, branch_name: &str) -> (Oid, Oid) {
+        let mut index = repo.index().unwrap();
+        let mut branch_ref_name = String::from("refs/heads/");
+        branch_ref_name.push_str(branch_name);
+
+        let tree_id = index.write_tree().unwrap();
+        let tree = repo.find_tree(tree_id).unwrap();
+        let sig = repo.signature().unwrap();
+        let oid = repo.refname_to_id(branch_ref_name.as_str()).unwrap();
+        let parent = repo.find_commit(oid).unwrap();
+        let commit = repo
+            .commit(
+                Some(branch_ref_name.as_str()),
+                &sig,
+                &sig,
+                "commit",
+                &tree,
+                &[&parent],
+            )
+            .unwrap();
+        (commit, tree_id)
+    }
+
+    fn create_branch_at_head(repo: &Repository, name: &str) {
+        let head = repo.head().unwrap();
+        let target = head.target().unwrap();
+        let commit = repo.find_commit(target).unwrap();
+        repo.branch(name, &commit, false).unwrap();
+    }
+
+    // Note: usually upstream wants to point to a remote, in which case you need to include the remote
+    // name in the upstream name e.g. "origin/mybranch"
+    fn set_branch_upstream(repo: &Repository, local_name: &str, upstream_name: Option<&str>) {
+        repo.find_branch(local_name, BranchType::Local)
+            .unwrap()
+            .set_upstream(upstream_name)
+            .unwrap();
+    }
+
+    #[test]
+    fn uncommitted() {
+        let (dir, repo) = repo_init();
+        assert!(!check_uncommitted(&repo).unwrap());
+
+        // Working directory
+        create_file(dir.path(), "file");
+        assert!(check_uncommitted(&repo).unwrap());
+
+        // Index
+        add_file_to_index(&repo, "file");
+        assert!(check_uncommitted(&repo).unwrap());
+    }
+
+    #[test]
+    fn stashed() {
+        let (dir, mut repo) = repo_init();
+        assert_eq!(check_stashed(&mut repo).unwrap(), 0);
+
+        create_file(dir.path(), "file1");
+        repo.stash_save(
+            &repo.signature().unwrap(),
+            "msg1",
+            Some(StashFlags::INCLUDE_UNTRACKED),
+        )
+        .unwrap();
+        assert_eq!(check_stashed(&mut repo).unwrap(), 1);
+
+        create_file(dir.path(), "file2");
+        repo.stash_save(
+            &repo.signature().unwrap(),
+            "msg2",
+            Some(StashFlags::INCLUDE_UNTRACKED),
+        )
+        .unwrap();
+        assert_eq!(check_stashed(&mut repo).unwrap(), 2);
+
+        repo.stash_drop(0).unwrap();
+        repo.stash_drop(0).unwrap();
+        assert_eq!(check_stashed(&mut repo).unwrap(), 0);
+    }
+
+    #[test]
+    fn ahead_behind() {
+        let (upstream_dir, upstream_repo) = repo_init();
+        let (local_dir, local_repo) = repo_init();
+        local_repo
+            .remote("origin", upstream_dir.path().to_str().unwrap())
+            .unwrap();
+
+        // Create branches on local and upstream
+        create_branch_at_head(&local_repo, "b1");
+        create_branch_at_head(&local_repo, "b2");
+        create_branch_at_head(&local_repo, "b3");
+        create_branch_at_head(&upstream_repo, "b1");
+        create_branch_at_head(&upstream_repo, "b2");
+        create_branch_at_head(&upstream_repo, "b3");
+
+        // Fetch the newly created branches from upstream. This is required in order to set
+        // these as upstream tracking branches for the local ones.
+        if let Ok(mut remote) = local_repo.find_remote("origin") {
+            let refspecs: &[&str] = &[];
+            remote
+                .fetch(refspecs, None, None)
+                .map_err(AheadBehindError)
+                .unwrap();
+        }
+
+        // Connect local and upstream branches
+        set_branch_upstream(&local_repo, "b1", Some("origin/b1"));
+        set_branch_upstream(&local_repo, "b2", Some("origin/b2"));
+        set_branch_upstream(&local_repo, "b3", Some("origin/b3"));
+
+        // Make local b1 ahead of upstream
+        create_file(local_dir.path(), "file1");
+        add_file_to_index(&local_repo, "file1");
+        commit_index_to_branch(&local_repo, "b1");
+
+        // Make upstream b2 ahead of local
+        create_file(upstream_dir.path(), "file2");
+        add_file_to_index(&upstream_repo, "file2");
+        commit_index_to_branch(&upstream_repo, "b2");
+
+        // Make b3 ahead and behind
+        create_file(local_dir.path(), "file3a");
+        add_file_to_index(&local_repo, "file3a");
+        commit_index_to_branch(&local_repo, "b3");
+
+        create_file(upstream_dir.path(), "file3b");
+        add_file_to_index(&upstream_repo, "file3b");
+        commit_index_to_branch(&upstream_repo, "b3");
+
+        let results: Vec<AheadBehind> = check_ahead_behind(&local_repo, "origin")
+            .unwrap()
+            .collect::<Result<_, _>>()
+            .unwrap();
+        assert_eq!(results.len(), 4);
+
+        assert!(results.contains(&AheadBehind {
+            ahead: None,
+            behind: None,
+            branch_name: Some("main".into()),
+            upstream_name: None,
+        }));
+        assert!(results.contains(&AheadBehind {
+            ahead: Some(1),
+            behind: Some(0),
+            branch_name: Some("b1".into()),
+            upstream_name: Some("origin/b1".into()),
+        }));
+        assert!(results.contains(&AheadBehind {
+            ahead: Some(0),
+            behind: Some(1),
+            branch_name: Some("b2".into()),
+            upstream_name: Some("origin/b2".into()),
+        }));
+        assert!(results.contains(&AheadBehind {
+            ahead: Some(1),
+            behind: Some(1),
+            branch_name: Some("b3".into()),
+            upstream_name: Some("origin/b3".into()),
+        }));
+    }
+
+    #[test]
+    fn hooks() {
+        let (dir, repo) = repo_init();
+        let results = check_hooks(&repo).unwrap();
+        assert!(results.is_empty());
+
+        let active_dir = dir.path().join(".git/hooks");
+        let in_repo_dir = dir.path().join(".githooks");
+
+        // Only in `.git/hooks`.
+        create_file(&active_dir, "hook.sample");
+        create_file(&active_dir, "hook1");
+        let results = check_hooks(&repo).unwrap();
+        assert_eq!(
+            results,
+            vec![Hook {
+                name: "hook1".into(),
+                state: HookState::ActiveOnly
+            }]
+        );
+        remove_file(&active_dir, "hook.sample");
+        remove_file(&active_dir, "hook1");
+
+        // Only in `.githooks`.
+        create_file(&in_repo_dir, "hook.sample");
+        create_file(&in_repo_dir, "hook1");
+        let results = check_hooks(&repo).unwrap();
+        assert_eq!(
+            results,
+            vec![Hook {
+                name: "hook1".into(),
+                state: HookState::InRepoOnly
+            }]
+        );
+        remove_file(&in_repo_dir, "hook.sample");
+        remove_file(&in_repo_dir, "hook1");
+
+        // In both locations but file contents don't match.
+        create_file_with_contents(&active_dir, "hook.sample", "a");
+        create_file_with_contents(&active_dir, "hook1", "b");
+        create_file_with_contents(&in_repo_dir, "hook.sample", "c");
+        create_file_with_contents(&in_repo_dir, "hook1", "d");
+        let results = check_hooks(&repo).unwrap();
+        assert_eq!(
+            results,
+            vec![Hook {
+                name: "hook1".into(),
+                state: HookState::Mismatch
+            }]
+        );
+        remove_file(&active_dir, "hook.sample");
+        remove_file(&active_dir, "hook1");
+        remove_file(&in_repo_dir, "hook.sample");
+        remove_file(&in_repo_dir, "hook1");
+
+        // In both locations and file contents match.
+        create_file(&active_dir, "hook.sample");
+        create_file(&active_dir, "hook1");
+        create_file(&in_repo_dir, "hook.sample");
+        create_file(&in_repo_dir, "hook1");
+        let results = check_hooks(&repo).unwrap();
+        assert_eq!(
+            results,
+            vec![Hook {
+                name: "hook1".into(),
+                state: HookState::Good
+            }]
+        );
+        remove_file(&active_dir, "hook.sample");
+        remove_file(&active_dir, "hook1");
+        remove_file(&in_repo_dir, "hook.sample");
+        remove_file(&in_repo_dir, "hook1");
+    }
 }
